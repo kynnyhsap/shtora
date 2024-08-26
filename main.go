17fe481e67dfc2aa14c1002f6a455205b86fdd0f
Andriy Pashynnyk <tobirawork@gmail.com>
2024-08-26 13:56:59 +0300

    add go server

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("./static"))
+
+	http.Handle("/", fs)
+
+	log.Println("server is running on http://localhost:3000")
+
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
